36a978c860a7f8116f6b51a2a86123c8cd9c0848
kizcibek <kizcibek@gmail.com>
2022-03-27 01:46:43 +0600

    my blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
-from blog.views import posts_list, PostDetail, tags_list, TagCreate, TagDetail
+from blog.views import *
 
 
-urlpatterns = [
-    path('', posts_list, name='post_list_url'),
+urlpatterns = {
+    path('', posts_list, name='posts_list_url'),
+    path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
 
-]
+}
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
-from .models import Post, Tag
-from .utils import ObjectDetailMixin
+from .models import Tag, Post
+from .utils import ObjectDetailMixin, ObjectCreateMixin
+from .forms import TagForm, PostForm
 
-from blog.forms import TagForm
 
-
-# Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
@@ -19,23 +17,19 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
 
 def tags_list(request):
