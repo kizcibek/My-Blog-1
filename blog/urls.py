e48049afa15457156df5f1362601e983a39429fd
kizcibek <kizcibek@gmail.com>
2022-03-26 15:26:33 +0600

    my blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import *
+
+from blog.views import posts_list, PostDetail, tags_list, TagCreate, TagDetail
+
 
 urlpatterns = [
     path('', posts_list, name='post_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url')
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
+
 ]
